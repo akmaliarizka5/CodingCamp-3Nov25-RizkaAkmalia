ecbc41cb1a39254e9f051e9ef33a11ea8685692c
Rizka Akmalia <akmaliarizka5@gmail.com>
2025-11-07 14:12:49 +0700

    add alert massage if name null

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@ welcomeMessage();
 function welcomeMessage() {
     let name = prompt("Please enter your name:");
 
-    if (name != null || name.trim() != "") {
+    if (name != null && name.trim() != "") {
         document.getElementById("welcome-speech").innerText = "Hello " + name + ", Welcome to our website!";
-    }
-    else {
-        document.getElementById("welcome-speech").innerText = "Please enter your name first!";
+    } else {
+        alert("Please enter your name first!");
     }
 }
